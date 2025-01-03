d5b1cff41880bcd76009d76b21dd6c5d78c1bc58
mwnseo <leeminseo0110@gmail.com>
2025-01-03 01:38:35 -0600

    delete demo button

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -3,20 +3,18 @@ import React from "react";
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
-export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source },
-}) => {
+export const ProjectCard = ({ project }) => {
   return (
     <div className={styles.container}>
       <img
-        src={getImageUrl(imageSrc)}
-        alt={`Image of ${title}`}
+        src={getImageUrl(project.imageSrc)}
+        alt={`Image of ${project.title}`}
         className={styles.image}
       />
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
+      <h3 className={styles.title}>{project.title}</h3>
+      <p className={styles.description}>{project.description}</p>
       <ul className={styles.skills}>
-        {skills.map((skill, id) => {
+        {project.skills.map((skill, id) => {
           return (
             <li key={id} className={styles.skill}>
               {skill}
@@ -25,10 +23,7 @@ export const ProjectCard = ({
         })}
       </ul>
       <div className={styles.links}>
-        <a href={demo} className={styles.link}>
-          Demo
-        </a>
-        <a href={source} className={styles.link}>
+        <a href={project.source} className={styles.link}>
           Source
         </a>
       </div>
